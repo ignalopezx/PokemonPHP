ea8a3b8c0680a75606aebff77ec796d54ea9cde8
Ezequiel Miranda <mirandaezequiel522@gmail.com>
2024-04-26 14:35:31 -0300

    crear index, separar header y search-bar, arreglar SQL

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokédex</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+        include("header.php");
+        include("search-bar.php");
+    ?>
+
+    <div class="resultados">
+        <?php
+        include "mostrar_tablas.php";
+        mostrar_pokemons();
+        
+        if (isset($_GET['buscar'])) {
+            mostrar_pokemons($_GET['buscar']);
+        }
+        ?>
+    </div>
+</body>
+</html>
+
+
---
+++ search-bar.php
@@ -0,0 +1,6 @@
+<div class="search-bar">
+        <form action="buscar.php" method="get">
+            <input type="text" name="buscar" placeholder="Buscar Pokémon">
+            <button type="submit">Buscar</button>
+        </form>
+    </div>
