618731ad9ed11e3fa29c2500f790202845c3ea67
Ezequiel Miranda <mirandaezequiel522@gmail.com>
2024-04-30 19:31:36 -0300

    update

---
+++ users.sql
@@ -0,0 +1,11 @@
+USE pokemons;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    numero_identificador INT UNIQUE NOT NULL,
+    usuario VARCHAR(30) NOT NULL,
+    contraseña VARCHAR(30) NOT NULL
+);
+
+INSERT INTO users (numero_identificador, usuario, contraseña) VALUES
+(1,"admin", "admin");
