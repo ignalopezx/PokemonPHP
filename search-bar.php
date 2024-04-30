a7d9d759557db8896f9edcf1cbc29975c3ac01b4
Santiago <142843041+SantiagoTucci@users.noreply.github.com>
2024-04-29 22:11:12 -0300

    Actualizacion

--- search-bar.php
+++ search-bar.php
@@ -1,6 +1,6 @@
 <div class="search-bar">
-    <form action="busqueda.php" method="get">
-        <input type="text" name="buscar" placeholder="Buscar Pokémon">
-        <button type="submit">Buscar</button>
+    <form class="form-busqueda" action="busqueda.php" method="get">
+        <input class="input-busqueda" type="text" name="buscar" placeholder="Buscar Pokémon">
+        <button class="btn-busqueda" type="submit">Buscar</button>
     </form>
 </div>
