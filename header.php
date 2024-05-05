4e498e9ae8eae14dd5646da18efc19ec9603143e
Ignacio Lopez Cabanillas <ignaciolopezcabanillas@gmail.com>
2024-05-05 17:33:01 -0300

    login

--- header.php
+++ header.php
@@ -1,3 +1,20 @@
+<?php
+
+if( isset($_GET["error"]) ){
+    switch ($_GET["error"]){
+        case 1:
+            echo "<div style='background-color: aquamarine;color:red' >Usuario y contraseña invalidos </div> ";
+            break;
+        case 2:
+            echo "<div style='background-color: aquamarine;color:red' >Debe completar los datos </div> ";
+            break;
+        case 3:
+            echo "<div style='background-color: aquamarine;color:red' >LTA </div> ";
+            break;
+    }
+}
+
+?>
 <header>
     <div class="logo">
         <a href="index.php"><img src="imagenes/pokeball-logo.png" alt="pokeball-logo" width="50px" height="50px"></a>
---
+++ login.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+
+if ( isset($_POST["usuario"]) &&  isset($_POST["contraseña"]) ){
+    $usuario = $_POST["usuario"];
+    $contraseña = $_POST["contraseña"];
+
+    $esValido = consultarBD($usuario, $contraseña);
+
+    if($esValido){
+        $_SESSION["usuario"] = $usuario;
+
+        header("location:index.php");
+        exit();
+    } else {
+        header("location:index.php?error=1");
+        exit();
+    }
+
+} else {
+    header("location:index.php?error=2");
+    exit();
+}
+
+function consultarBD($usuario, $contraseña)
+{
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "pokemons";
+
+    // Crear conexión
+    $conn = mysqli_connect($servername, $username, $password, $database);
+
+    // Verificar la conexión
+    if (!$conn) {
+        die("Error al conectar con la base de datos: " . mysqli_connect_error());
+    }
+
+    // Realizar consulta
+    $sql = "SELECT * FROM users WHERE usuario = '" . $usuario . "' && contraseña = '" . $contraseña . "'";
+    $result = mysqli_query($conn, $sql);
+
+    return mysqli_num_rows($result) == 1;
+}
